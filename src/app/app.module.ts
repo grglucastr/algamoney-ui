47f73af83b02b651ac328c9693b34c48beba102a
George Bentes <george.bentes@gmail.com>
2019-10-09 22:18:20 -0400

    Fix error with FontAwesome lib and test Prime NG installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TabViewModule} from 'primeng/tabview';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
