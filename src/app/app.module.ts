771cabfd35b2a4c3beaa5cfb7110d7ff7e773cf1
George Bentes <george.bentes@gmail.com>
2019-10-09 22:21:48 -0400

    Remove HelloComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {TabViewModule} from 'primeng/tabview';
-import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
   ],
   imports: [
     BrowserModule, TabViewModule
