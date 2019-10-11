aac4e796d58c05a0be87effe2d08ecb0213e9be7
George Bentes <george.bentes@gmail.com>
2019-10-10 21:39:06 -0400

    Add component 'lancamentos-pesquisa' due refactor reasons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
 import { TableModule } from 'primeng/components/table/table';
 import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LancamentosPesquisaComponent,
   ],
   imports: [
     BrowserModule,
